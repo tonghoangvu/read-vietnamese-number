3e90fc6f7fe57f04cb150faad3fac91816fdad5c
tonghoangvu <tonghoangvu@outlook.com>
2021-02-21 12:03:49 +0700

    Thêm ESLint vào project

---
+++ .eslintrc.js
@@ -0,0 +1,87 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'commonjs': true,
+        'es6': true,
+        'node': true
+    },
+    'extends': 'eslint:recommended',
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parserOptions': {
+        'ecmaVersion': 2020
+    },
+    'rules': {
+        'no-console': [
+            'warn'
+        ],
+        'no-debugger': [
+            'warn'
+        ],
+        'indent': [
+            'warn',
+            4,
+            {
+                'SwitchCase': 1,
+                'FunctionDeclaration': {
+                    'body': 1,
+                    'parameters': 2
+                }
+            }
+        ],
+        'linebreak-style': [
+            'warn',
+            'unix'
+        ],
+        'quotes': [
+            'warn',
+            'single'
+        ],
+        'semi': [
+            'warn',
+            'always'
+        ],
+        'max-len': [
+            'warn',
+            {
+                'code': 100,
+                'ignoreUrls': true
+            }
+        ],
+        'no-trailing-spaces': [
+            'warn'
+        ],
+        'eol-last': [
+            'warn',
+            'always'
+        ],
+        'no-multiple-empty-lines': [
+            'warn'
+        ],
+        'brace-style': [
+            'warn',
+            '1tbs',
+            {
+                'allowSingleLine': true
+            }
+        ],
+        'comma-dangle': [
+            'warn',
+            'never'
+        ],
+        'no-var': [
+            'warn'
+        ],
+        'prefer-const': [
+            'warn'
+        ],
+        'space-in-parens': [
+            'warn'
+        ],
+        'no-use-before-define': [
+            'warn'
+        ]
+    }
+};

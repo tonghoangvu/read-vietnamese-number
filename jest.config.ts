4fe7b4faf6af79f014f0318b4bb20fd2c041199a
Vu Tong <tonghoangvu@outlook.com>
2023-02-22 16:46:19 +0700

    bug: Jest cannot find module when using ESM import

--- jest.config.ts
+++ jest.config.ts
@@ -3,10 +3,17 @@ import type { Config } from 'jest'
 const config: Config = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
+
+	// https://kulshekhar.github.io/ts-jest/docs/guides/esm-support/
+	extensionsToTreatAsEsm: ['.ts'],
+	moduleNameMapper: {
+		'^(\\.{1,2}/.*)\\.js$': '$1',
+	},
 	transform: {
-		'\\.ts$': [
+		'^.+\\.tsx?$': [
 			'ts-jest',
 			{
+				useESM: true,
 				tsconfig: {
 					// To use bigint literals in tests
 					target: 'ES2020',
